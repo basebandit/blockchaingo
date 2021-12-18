685b6a8728a960a54285fbf25f6789a73418fe36
Evanson Mwangi <evansonmwangi83@gmail.com>
2021-12-18 21:54:01 +0300

    replace set hash with proof-of-work

--- block.go
+++ block.go
@@ -1,9 +1,6 @@
 package blockchaingo
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"strconv"
 	"time"
 )
 
@@ -17,20 +14,26 @@ type Block struct {
 	PrevBlockHash []byte
 	//Hash is the hash of the block.
 	Hash []byte
+	//Nonce arbitrary number to be used once.
+	Nonce int
 }
 
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
+// func (b *Block) SetHash() {
+// 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+// 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+// 	hash := sha256.Sum256(headers)
 
-	b.Hash = hash[:]
-}
+// 	b.Hash = hash[:]
+// }
 
 //NewBlock creates a new block
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
 
 	return block
 }
